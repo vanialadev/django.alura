b12d04d52eaddc398e6ea76414876d3f54e084b1
Vania Almeida <vanialadev@gmail.com>
2016-12-29 03:29:23 -0300

    criando a página de um perfil

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from models import Perfil
+
 
 def index(request):
-    #return HttpResponse('Bem vindo ao ConnectedIN')
     return render(request, 'index.html')
+
+
+def exibir(request, perfil_id):
+    perfil = Perfil()
+    if perfil_id == '1':
+        perfil = Perfil('Flavio Almeida', 'xxxxxxx', '777777', 'Caelum')
+    if perfil_id == '2':
+        perfil = Perfil('Romulo Henrique', 'R@R.com', '888888', 'Caelum')
+
+    return render(request, 'perfil.html', {"perfil": perfil})
