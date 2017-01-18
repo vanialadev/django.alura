2182a1c5f051f9d1876573b4e232eec7190f2b40
Vania Almeida <vanialadev@gmail.com>
2017-01-18 18:14:54 -0300

    exibindo convites recebidos

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -3,12 +3,11 @@ from models import Perfil
 
 
 def index(request):
-    return render(request, 'index.html', {'perfis': Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
-
     return render(request, 'perfil.html', {"perfil": perfil})
 
 def convidar(request, perfil_id):
