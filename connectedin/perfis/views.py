c9933051cafbab508a4f4de8cf5e6b11b43c63ea
Vania Almeida <vanialadev@gmail.com>
2017-01-18 16:44:12 -0300

    convidando perfis

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import Perfil
 
 
@@ -12,5 +12,11 @@ def exibir(request, perfil_id):
     return render(request, 'perfil.html', {"perfil": perfil})
 
 def convidar(request, perfil_id):
-    pass
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    #return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+    return redirect('index')
 
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
