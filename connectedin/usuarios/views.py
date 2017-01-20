bcdccca8d6389ea640378eb2b4d414540e9048e6
Vania Almeida <vanialadev@gmail.com>
2017-01-20 17:03:51 -0300

    persistindo perfis e validando formularios

--- connectedin/usuarios/views.py
+++ connectedin/usuarios/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
+from usuarios.form import RegistrarUsuarioForm
+from django.contrib.auth.models import User
+from perfis.models import Perfil
 
 
 class RegistrarUsuarioView(View):
@@ -9,5 +12,32 @@ class RegistrarUsuarioView(View):
     def get(self, request):
         return render(request, self.template_name)
 
-    def set(self, request):
-        return render(request, self.template_name)
+    def post(self, request):
+
+        #preenche o from
+        form = RegistrarUsuarioForm(request.POST)
+
+        #verifica se eh valido
+        if form.is_valid():
+
+            dados_form = form.data
+
+            #cria o usuario
+            usuario = User.objects.create_user(dados_form['nome'], dados_form['email'], dados_form['senha'])
+
+            #cria o perfil
+            perfil = Perfil(nome=dados_form['nome'],
+                            email=dados_form['email'],
+                            telefone=dados_form['telefone'],
+                            nome_empresa=dados_form['nome_empresa'],
+                            usuario=usuario)
+
+            #grava no banco
+            perfil.save()
+
+            #redireciona para index
+            return redirect('index')
+
+        #so chega aqui se nao for valido
+        #vamos devolver o form para mostrar o formulario preenchido
+        return render(request, self.template_name, {'form' : form})
